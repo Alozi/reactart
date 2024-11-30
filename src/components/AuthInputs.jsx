211a0f64e7ae9b728404a9cbb76737e452edf5bd
Alozi <alozimozi@gmail.com>
2024-11-30 10:12:21 +0200

    Migrate all app to Tailwind Css

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
-import { styled } from "styled-components";
+// import { styled } from "styled-components";
 
 import Button from "./Button";
 import CustomInput from "./CustomInput";
 
-const ControlsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
+// const ControlsContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `;
 
-const ButtonText = styled.button`
-  color: #f0b322;
-  border: none;
-`;
+// const ButtonText = styled.button`
+//   color: #f0b322;
+//   border: none;
+// `;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -37,8 +37,12 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlsContainer>
+    <div
+      // id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      {/* <ControlsContainer> */}
+      <div className="flex flex-col gap-2 mb-6">
         <CustomInput
           label="Email"
           type="email"
@@ -78,9 +82,12 @@ export default function AuthInputs() {
             }
           />
         </p> */}
-      </ControlsContainer>
-      <div className="actions">
-        <ButtonText>Create a new account</ButtonText>
+        {/* </ControlsContainer> */}
+      </div>
+      {/* <div className="actions"> */}
+      <div className="flex justify-end gap-4">
+        {/* <ButtonText>Create a new account</ButtonText> */}
+        <button className="text-amber-400 hover:text-amber-500">Create a new account</button>
         <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
